d0d152bf0b4d4cebb1993d93c6fe4f6a3822efe2
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-02 16:54:31 -0500

    Updated routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 
+import AdminController from './app/controllers/AdminController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'hello, world' }));
+routes.get('/admins', AdminController.index);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
