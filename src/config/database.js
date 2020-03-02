10c9f8782af29fbee935554ddeb82cd86d005657
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-02 14:52:41 -0500

    Initial configuration

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'postgress',
+  database: 'challenge03',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
