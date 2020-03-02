918fd87350542a076c920a51a6e08f436d98b25d
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-02 17:07:13 -0500

    Added authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import AdminController from './app/controllers/AdminController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/admins', AdminController.index);
