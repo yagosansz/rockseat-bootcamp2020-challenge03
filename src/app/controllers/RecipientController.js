61bdd45a65f548809bd372394e3b6bfb34fb39f6
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-03 11:22:09 -0500

    Added schema validation

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,26 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object.shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      street_name: Yup.string().required(),
+      street_number: Yup.string().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      postal_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const { email } = req.body;
 
     const recipientExists = await Recipient.findOne({
@@ -18,6 +37,21 @@ class RecipientController {
   }
 
   async edit(req, res) {
+    const schema = Yup.object.shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      street_name: Yup.string(),
+      street_number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      postal_code: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const { email } = req.body;
 
     let recipient = await Recipient.findOne({ where: { email } });
