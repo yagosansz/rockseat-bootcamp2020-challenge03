e691e51c0fdc7d43a5cd9b59bfe179e95f6954a3
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-03 13:32:36 -0500

    Added file upload

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -4,9 +4,14 @@ class File extends Model {
   static init(sequelize) {
     super.init(
       {
-        id: Sequelize.INTENGER,
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import AdminController from './app/controllers/AdminController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // List admins in the database
 routes.get('/admins', AdminController.index);
@@ -16,5 +20,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.post('/recipients', authMiddleware, RecipientController.store);
 routes.put('/recipients', authMiddleware, RecipientController.edit);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
