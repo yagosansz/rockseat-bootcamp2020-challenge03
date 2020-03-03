61588b6c1707c1e367cc2e3513f19269b02e981b
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-03 17:36:27 -0500

    Updated models loading to include association

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
+import Deliveryman from '../app/models/Deliveryman';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File];
+const models = [User, Recipient, File, Deliveryman];
 
 class Database {
   constructor() {
@@ -17,7 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
