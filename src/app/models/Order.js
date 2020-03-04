4204c6dc82a9d2fec8f1371e79eb85832cfa8413
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-04 16:09:38 -0500

    Added Order model

---
+++ src/app/models/Order.js
@@ -0,0 +1,35 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Order extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        recipient_id: Sequelize.INTEGER,
+        deliveryman_id: Sequelize.INTEGER,
+        signature_id: Sequelize.INTEGER,
+        product: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Recipient, {
+      foreignKey: 'recipient_id',
+      as: 'recipient',
+    });
+    this.belongsTo(models.Deliveryman, {
+      foreignKey: 'deliveryman_id',
+      as: 'deliveryman',
+    });
+    this.belongsTo(models.File, {
+      foreignKey: 'signature_id',
+      as: 'signature',
+    });
+  }
+}
+
+export default Order;
