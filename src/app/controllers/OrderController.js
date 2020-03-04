ff072c5bcec111a4c76025bf98d9b649a08932b8
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-04 17:18:25 -0500

    Created OrderController with CRUD operations

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,89 @@
+import Order from '../models/Order';
+import Recipient from '../models/Recipient';
+import Deliveryman from '../models/Deliveryman';
+
+class OrderController {
+  async index(req, res) {
+    // needs to edit findAll to display more data about recipient and deliveryman
+    const orders = await Order.findAll({});
+
+    if (!orders.length) {
+      return res
+        .status(400)
+        .json({ error: 'No orders have been registered in the system yet' });
+    }
+
+    return res.json(orders);
+  }
+
+  async store(req, res) {
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const recipientExists = await Recipient.findByPk(recipient_id);
+
+    if (!recipientExists) {
+      return res.status(400).json({ error: 'Recipient does not exist' });
+    }
+
+    const deliverymanExists = await Deliveryman.findByPk(deliveryman_id);
+
+    if (!deliverymanExists) {
+      return res.status(400).json({ error: 'Deliveryman does not exist' });
+    }
+
+    const order = await Order.create(req.body);
+
+    return res.json(order);
+  }
+
+  async edit(req, res) {
+    const { id } = req.params;
+    const { recipient_id, deliveryman_id } = req.body;
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      res.status(404).json({ error: 'Could not find order' });
+    }
+
+    if (recipient_id && recipient_id !== order.recipient_id) {
+      const recipientExists = await Recipient.findByPk(recipient_id);
+
+      if (!recipientExists) {
+        return res.status(400).json({ error: 'Recipient does not exist' });
+      }
+    }
+
+    if (deliveryman_id && deliveryman_id !== order.deliveryman_id) {
+      const deliverymanExists = await Deliveryman.findByPk(deliveryman_id);
+
+      if (!deliverymanExists) {
+        return res.status(400).json({ error: 'Deliveryman does not exist' });
+      }
+    }
+
+    const { product } = await order.update(req.body);
+
+    return res.json({
+      product,
+      recipient_id,
+      deliveryman_id,
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const order = await Order.findByPk(id);
+
+    if (!order) {
+      res.status(404).json({ error: 'Could not find order' });
+    }
+
+    await order.destroy();
+
+    return res.send();
+  }
+}
+
+export default new OrderController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import OrderController from './app/controllers/OrderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,19 +20,30 @@ routes.get('/admins', AdminController.index);
 // Create a session for users trying to authenticate
 routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+
 // Creates a recipient
-routes.post('/recipients', authMiddleware, RecipientController.store);
+routes.post('/recipients', RecipientController.store);
 // Edits a recipient
-routes.put('/recipients', authMiddleware, RecipientController.edit);
+routes.put('/recipients', RecipientController.edit);
 
 // Lists deliverymen
-routes.get('/deliverymen', authMiddleware, DeliverymanController.index);
+routes.get('/deliverymen', DeliverymanController.index);
 // Stores a deliverymen
-routes.post('/deliverymen', authMiddleware, DeliverymanController.store);
+routes.post('/deliverymen', DeliverymanController.store);
 // Updates a deliverymen
-routes.put('/deliverymen/:id', authMiddleware, DeliverymanController.edit);
+routes.put('/deliverymen/:id', DeliverymanController.edit);
 // Removes a deliverymen
-routes.delete('/deliverymen/:id', authMiddleware, DeliverymanController.delete);
+routes.delete('/deliverymen/:id', DeliverymanController.delete);
+
+// List all registered orders
+routes.get('/orders', OrderController.index);
+// Store new order
+routes.post('/orders', OrderController.store);
+// Updates an existing order
+routes.put('/orders/:id', OrderController.edit);
+// Deletes an existing order
+routes.delete('/orders/:id', OrderController.delete);
 
 // Uploads file to ./tmp/uploads
 routes.post('/files', upload.single('file'), FileController.store);
